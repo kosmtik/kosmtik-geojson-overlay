533a889585c2aed6f7aac84c504ec4e8a01e4f3b
Hidde Wieringa <hidde@hiddewieringa.nl>
2020-09-12 19:19:24 +0200

    Use class syntax
    Ref https://github.com/kosmtik/kosmtik/pull/319

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-exports.Plugin = function (config) {
-    config.addJS('/node_modules/kosmtik-geojson-overlay/front.js');
-};
+class GeoJsonOverlay {
+    constructor(config) {
+        config.addJS('/node_modules/kosmtik-geojson-overlay/front.js');
+    }
+}
+
+exports = { Plugin: GeoJsonOverlay }
